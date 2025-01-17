07ab9cd15e532b53463f8f85649e300adfc16f22
kpookgwadi023b <kpookgwadi023b@student.wethinkcode.co.za>
2025-01-17 13:30:04 +0200

    Added the repetition functionality

--- src/main/hangman.py
+++ src/main/hangman.py
@@ -4,14 +4,25 @@ word_list = ["beekeper", "geek", "orange", "order", "software"]
 
 chosen_word = word_list[random.randint(0, len(word_list) -1)]
 
-guess = ["_"]*len(chosen_word)
+display = ["_"]*len(chosen_word)
 
 print(chosen_word)
-guessed_char  = input("Guess a letter: ").lower()
+
+end_of_game = False
+
+while not end_of_game:
+    guessed_char  = input("display a letter: ").lower()
 
 
-for i in range(len(chosen_word)):
-    if chosen_word[i] == guessed_char:
-        guess[i] = guessed_char
-        
-print(guess)
+    for i in range(len(chosen_word)):
+        if chosen_word[i] == guessed_char:
+            display[i] = guessed_char
+            
+    print(display)
+    
+    if "_" not in display:
+        end_of_game = True
+    
+    
+    
+print("You win!")
