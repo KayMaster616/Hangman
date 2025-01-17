3cdd9e16515dc9cb398ecdd5952a9e7ad014bb5e
kpookgwadi023b <kpookgwadi023b@student.wethinkcode.co.za>
2025-01-17 15:20:06 +0200

    Added the hangman ascii functionality

--- src/main/hangman.py
+++ src/main/hangman.py
@@ -10,19 +10,99 @@ print(chosen_word)
 
 end_of_game = False
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+# print(stages[5])
+
+count_chars = len(display)
+current_chars = count_chars
+lives = 6
 while not end_of_game:
+    
     guessed_char  = input("display a letter: ").lower()
 
-
-    for i in range(len(chosen_word)):
-        if chosen_word[i] == guessed_char:
-            display[i] = guessed_char
+    if guessed_char in display:
             
-    print(display)
+        lives -= 1
+        print("You already guessed this letter")
+        if lives == 0:
+            end_of_game = True
+            print("Sorry, You lost!")
+                
+    else:
     
-    if "_" not in display:
-        end_of_game = True
+        for i in range(len(chosen_word)):
+            if chosen_word[i] == guessed_char:
+                display[i] = guessed_char
+                
+        if guessed_char in chosen_word:
+            
+            if "_" not in display:
+                end_of_game = True
+                print("Congrats, You win!")
+        
+        else:
+            lives -= 1
+            print("Your guess is wrong")
+            if lives == 0:
+                end_of_game = True
+                print("Sorry, You lost!")
+  
+    print(" ".join(display))        
+    print(stages[lives])        
     
-    
-    
-print("You win!")
